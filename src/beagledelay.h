9dd296e78e58fd6bb3305417f8eca4adf2031b6c
Markus Klink <justjoheinz@gmail.com>
2012-03-21 17:33:32 +0100

    added documentation

--- src/beagledelay.h
+++ src/beagledelay.h
@@ -1,8 +1,15 @@
+/**
+ * @file beagledelay
+ * @brief Utilities to delay program execution
+ */
 #ifndef _BEAGLEDELAY_H
 #define _BEAGLEDELAY_H
 
 #include <unistd.h>
 
+/**
+ * delay in microsencods.
+ */
 extern int delayMicro(useconds_t useconds);
 
 #endif /* _BEAGLEDELAY_H */
